813300d51d59fd8c696f4b76d69d959a150069d5
gaurang-knight007 <149990199+gaurang-knight007@users.noreply.github.com>
2025-03-23 03:56:23 +0530

    Update Index3.jsx
    environment fixed

--- src/pages/index3/Index3.jsx
+++ src/pages/index3/Index3.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Index3.css";
 import logo from "./logo1.png";
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 const Index3 = () => {
   const [formData, setFormData] = useState({
     name: "",
